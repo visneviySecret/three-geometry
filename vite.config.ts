6b7a0a2d3bfaab7ca7206b6aef884c24d15feb9d
E. Belousov <visneviy.secret@yandex.ru>
2025-05-23 18:42:43 +0300

    Fix build dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from "path";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,7 +12,7 @@ export default defineConfig({
   base: "/three-geometry/",
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": resolve(__dirname, "./src"),
     },
   },
 });
